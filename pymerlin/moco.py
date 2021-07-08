8c67d4080eb19e57ef454949712fe411aadb4f88
Emil Ljungberg <ljungberg.emil@gmail.com>
2021-07-08 17:23:27 +0100

    SW moco and traj fix

--- pymerlin/moco.py
+++ pymerlin/moco.py
@@ -21,9 +21,9 @@ def calc_H(traj, D, spacing):
     dy = D['dy']/spacing[1]
     dz = D['dz']/spacing[2]
 
-    xF = traj[:, :, 0]/np.max(traj[:, :, 0])/2
-    yF = traj[:, :, 1]/np.max(traj[:, :, 1])/2
-    zF = traj[:, :, 2]/np.max(traj[:, :, 2])/2
+    xF = traj[:, :, 0]/np.max(abs(traj[:, :, 0]))/2
+    yF = traj[:, :, 1]/np.max(abs(traj[:, :, 1]))/2
+    zF = traj[:, :, 2]/np.max(abs(traj[:, :, 2]))/2
 
     H = np.exp(2j*np.pi*(xF*dx + yF*dy + zF*dz))
 
@@ -204,6 +204,90 @@ def moco_combined(source_h5, dest_h5, reg_list):
     logging.info("Writing k-space data")
     f_dest.create_dataset("noncartesian", dtype='c8', data=data_corr[np.newaxis, ...],
                           chunks=np.shape(data_corr), compression='gzip')
+
+    logging.info("Closing all files")
+    f_source.close()
+    f_dest.close()
+
+
+def moco_sw(source_h5, dest_h5, reg_list, nseg):
+    """Corrects radial dataset from a sliding window reconstruction
+
+    Args:
+        source_h5 (str): Source file to correct
+        dest_h5 (str): Output file
+        reg_list (list): List of registration dictionaries
+        nseg (int): Number of segments per interleave (equivalent to sliding window step)
+    """
+
+    # Load data
+    logging.info("Opening source file: %s" % source_h5)
+    f_source = h5py.File(source_h5, 'r')
+
+    info = f_source['info'][:]
+    spacing = info['voxel_size'][0]
+    spokes_lo = info['spokes_lo'][0]
+
+    n_nav = len(reg_list)
+
+    traj = f_source['trajectory'][:]
+    traj_corr = np.copy(traj)
+
+    data = f_source['noncartesian'][:]
+    data_corr = np.copy(data)
+
+    # We don't correct any lowres spokes
+    idx0 = 0
+    idx1 = int(spokes_lo)
+
+    logging.info("Correcting data and trajectories")
+    # Loop over all segments
+    for i in range(n_nav):
+        logging.info("Processing segment %d/%d" % (i+1, n_nav))
+
+        iw = i - int(np.floor(nseg/2))
+        if iw < 0:
+            iw = 0
+
+        D_reg = reg_list[iw]
+
+        idx0 = idx1              # Start where last interleave ended
+        sps = int(D_reg['spi']/nseg)  # Spokes per segment
+        idx1 = idx0 + sps
+        print("i0:i1: {}:{}".format(idx0, idx1))
+
+        traj_int = traj[idx0:idx1, :, :]
+        data_int = data[0, idx0:idx1, :, :]
+
+        print("Matmul")
+        traj_corr[idx0:idx1, :, :] = np.matmul(traj_int, D_reg['R'])
+
+        H = calc_H(traj_int, D_reg, spacing)
+        for ircv in range(np.shape(data)[-1]):
+            data_corr[0, idx0:idx1, :, ircv] = data_int[:, :, ircv]*H
+
+    # Write data to destination file
+    valid_dest_h5 = check_filename(dest_h5)
+    logging.info("Opening destination file: %s" % valid_dest_h5)
+    f_dest = h5py.File(valid_dest_h5, 'w')
+
+    logging.info("Writing info and meta data")
+    f_dest.create_dataset("info", data=info)
+    f_source.copy('meta', f_dest)
+
+    logging.info("Writing trajectory")
+    traj_chunk_dims = list(traj_corr.shape)
+    if traj_chunk_dims[0] > 1024:
+        traj_chunk_dims[0] = 1024
+    f_dest.create_dataset("trajectory", data=traj_corr,
+                          chunks=tuple(traj_chunk_dims), compression='gzip')
+
+    logging.info("Writing k-space data")
+    data_chunk_dims = list(data_corr.shape)
+    if data_chunk_dims[1] > 1024:
+        data_chunk_dims[1] = 1024
+    f_dest.create_dataset("noncartesian", dtype='c8', data=data_corr,
+                          chunks=tuple(data_chunk_dims), compression='gzip')
 
     logging.info("Closing all files")
     f_source.close()
