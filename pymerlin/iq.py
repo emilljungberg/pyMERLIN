94442d753845c58e9efedc6dd948f76a96b69006
Emil Ljungberg <ljungberg.emil@gmail.com>
2021-09-02 15:18:18 +0100

    Defaults from Wang

--- pymerlin/iq.py
+++ pymerlin/iq.py
@@ -31,12 +31,14 @@ def ssim(img1, img2, kw=11, sigma=0):
     img2 /= np.quantile(img2[...], 0.99)
 
     gauss_window = False
+    use_sample_covariance = True
     if sigma:
         gauss_window = True
+        use_sample_covariance = False
 
     mssim, S = structural_similarity(
         img1, img2, win_size=kw, data_range=1, gradient=False,
-        multichannel=False, gaussian_weights=gauss_window, full=True, use_sample_covariance=True,
+        multichannel=False, gaussian_weights=gauss_window, full=True, use_sample_covariance=use_sample_covariance,
         sigma=sigma)
 
     return mssim, S
